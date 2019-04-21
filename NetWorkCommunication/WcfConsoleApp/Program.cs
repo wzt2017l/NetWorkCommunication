177473a0cf7a98f1c1f7c97735caf07347e11000
wzt2017 <835726891@qq.com>
2019-04-21 10:24:39 +0800

    添加wcfdemo1

---
+++ NetWorkCommunication/WcfConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //JjccService.JjccServiceClient client = new JjccService.JjccServiceClient();
+            //var task = client.DiviAsync(11.321, 12.321);
+            //Console.WriteLine(client.Divi(1, 0));
+            //Console.WriteLine(client.Muit(10.5,1.32));
+            //Console.WriteLine(task.Result);
+            //client.Close();
+            //Console.Read();
+        }
+    }
+}
