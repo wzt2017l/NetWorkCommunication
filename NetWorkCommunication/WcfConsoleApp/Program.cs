ad381b1139294aaaa141402f426ca4c6d31142b5
wzt2017 <835726891@qq.com>
2019-04-21 11:47:26 +0800

    demo1

--- NetWorkCommunication/WcfConsoleApp/Program.cs
+++ NetWorkCommunication/WcfConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +11,8 @@ namespace WcfConsoleApp
     {
         static void Main(string[] args)
         {
-            //JjccService.JjccServiceClient client = new JjccService.JjccServiceClient();
-            //var task = client.DiviAsync(11.321, 12.321);
-            //Console.WriteLine(client.Divi(1, 0));
-            //Console.WriteLine(client.Muit(10.5,1.32));
-            //Console.WriteLine(task.Result);
-            //client.Close();
-            //Console.Read();
+          
         }
     }
+   
 }
