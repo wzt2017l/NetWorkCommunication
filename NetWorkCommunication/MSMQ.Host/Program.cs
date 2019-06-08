458235f0a97f4d2ef5a7d27f5bd572e14aca29e7
wzt2017 <835726891@qq.com>
2019-06-08 21:34:17 +0800

    MSMQ1

---
+++ NetWorkCommunication/MSMQ.Host/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Messaging;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSMQ.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uri = new Uri("net.msmq://localhost/private/WcfMsmqExampleTransacted");//队列侦听地址
+            Uri baseUri = new Uri("http://localhost:8933/Design_Time_Addresses/AirportService/");
+            string q1 = ConfigurationManager.AppSettings["queueNameTransacted"];
+            if (!MessageQueue.Exists(q1)) MessageQueue.Create(q1,true);
+            using (ServiceHost host = new ServiceHost(typeof(MSMQ.Service.AirportService)))
+            {
+                NetMsmqBinding msmqBinding = new NetMsmqBinding();
+                msmqBinding.Security.Mode = NetMsmqSecurityMode.None;
+                host.AddServiceEndpoint(typeof(MSMQ.Service.IAirportService), msmqBinding,baseUri,uri);
+                host.Open();
+                foreach (var v in host.Description.Endpoints)
+                {
+                    Console.WriteLine($"服务已启动{v.ListenUri.ToString()}");
+                }
+                Console.ReadKey();
+               
+            }
+        }
+    }
+}
---
+++ NetWorkCommunication/MSMQ.Service/AirportService.svc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MSMQ.Service
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“AirportService”。
+    // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 AirportService.svc 或 AirportService.svc.cs，然后开始调试。
+    public class AirportService : IAirportService
+    {
+        [OperationBehavior(TransactionScopeRequired =true,TransactionAutoComplete =true)]
+        public void SubmitInfo(string info)
+        {
+            Console.WriteLine($"收到:{info}");
+        }
+        [OperationBehavior(TransactionScopeRequired = true, TransactionAutoComplete = true)]
+        public void SubmitAirportMessage(AirportMessage message)
+        {
+            Console.WriteLine($"收到:{message.OriginalMessage}");
+            AirportMessages.Add(message);
+        }
+    }
+    [DataContract(Namespace ="WcfMsmqExamples")]
+    public class AirportMessage
+    {
+        public string AirportId;
+        public DateTime ForecastTime;
+        public string ShortMessage;
+        public string MessageId
+        {
+            get
+            {
+                return $"{AirportId},{ForecastTime:yyMM HHmm}";
+            }
+        }
+        public string OriginalMessage
+        {
+            get
+            {
+                return $"{MessageId} {ShortMessage}";
+            }
+        }
+        public string Status { get; internal set; }
+    }
+    public class AirportMessages
+    {
+        static Dictionary<string, AirportMessage> messages = new Dictionary<string, AirportMessage>();
+        public static void Add(AirportMessage message)
+        {
+            if (!messages.ContainsKey(message.AirportId))
+            {
+                messages.Add(message.AirportId,message);
+            }
+        }
+        public static void DeleteMessage(string id)
+        {
+            if (messages[id]!=null)
+            {
+                messages.Remove(id);
+            }
+        }
+    }
+}
